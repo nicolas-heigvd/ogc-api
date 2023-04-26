05fa1a9995256085f45ce2e845598f4a5d9d8ef5
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2023-04-26 11:56:48 +0200

    Minor fix

--- utils/convert.js
+++ utils/convert.js
@@ -3,22 +3,22 @@ function mod(n, m) {
 }
 
 const composeGeopose = (row, config) => {
-if (row['geojson']) {
-    const coordinate = JSON.parse(row['geojson']).coordinates
-    return {
-      "position": {
-        "lon": coordinate[0],
-        "lat": coordinate[1],
-        "h": coordinate[2]
-      },
-      "angles": {
-        "yaw": config.data.is_azimuth ? mod(360 - row[config.data.yaw_field], 360) : row[config.data.yaw_field],
-        "pitch": row[config.data.pitch_field],
-        "roll": row[config.data.roll_field]
+  if (row['geojson']) {
+      const coordinate = JSON.parse(row['geojson']).coordinates
+      return {
+        "position": {
+          "lon": coordinate[0],
+          "lat": coordinate[1],
+          "h": coordinate[2]
+        },
+        "angles": {
+          "yaw": config.data.is_azimuth ? mod(360 - row[config.data.yaw_field], 360) : row[config.data.yaw_field],
+          "pitch": row[config.data.pitch_field],
+          "roll": row[config.data.roll_field]
+        }
       }
-    }
-}
-return null;
+  }
+  return null;
 }
 
 module.exports = composeGeopose;
